d9f229352302c3cfe343a2d0db92c252b3048c15
nbond211 <nbond211@verizon.net>
2016-08-13 10:24:30 -0400

    chart.js cdn

--- static/js/main.js
+++ static/js/main.js
@@ -2,5 +2,9 @@ function analyzeSentiment(tweetId) {
     console.log($('#tweet-' + tweetId).text());
     $.post("/sentiment",
            {'text': $('#tweet-' + tweetId).text()},
-           function(data) {console.log(data);});
+           function(data) {console.log(createVisualization(data.positive, data.negative));});
+}
+
+function createVisualization(positive, negative) {
+    
 }
